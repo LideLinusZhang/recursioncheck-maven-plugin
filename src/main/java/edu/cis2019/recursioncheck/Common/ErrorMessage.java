4ce572d300e989bc912d1f2bd1fa759050af4d79
Lide Zhang <zhanglide2018@gmail.com>
2019-08-08 16:39:32 +0800

    cleanup

--- src/main/java/edu/cis2019/recursioncheck/Common/ErrorMessage.java
+++ src/main/java/edu/cis2019/recursioncheck/Common/ErrorMessage.java
@@ -3,10 +3,9 @@ package edu.cis2019.recursioncheck.Common;
 public enum ErrorMessage {
 	NO_BASE_CASE("This recursive method contains no base case"),
 	MUTUAL_RECURSIVE_WARNING("This method may be mutually recursive with another method"),
-	PARAMETERS_UNCHANGED("Parameters are unchanged from initial call to recursive call"),
-	;
-    
-	private ErrorMessage(String message) {
+    PARAMETERS_UNCHANGED("Parameters are unchanged from initial call to recursive call");
+
+    ErrorMessage(String message) {
 		this.errorMessage = message;
 	}
 
